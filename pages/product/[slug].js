02ab763ef8553281305723f43f6bdfa5f855094f
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-04-06 14:44:54 +0100

    Update [slug].js

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -56,7 +56,7 @@ export default function ProductScreen(props) {
         const existItem = cart.cartItems.find((x) => x._id === product._id);
         const quantity = existItem ? existItem.quantity + 1 : 1;
         const { data } = await axios.get(`/api/products/${product._id}`);
-        if (data.countInStock < 1) {
+        if (product.countInStock < 1) {
             enqueueSnackbar('Lamentamos. Os bilhetes para esta data estão esgotados.', { variant: 'error' });
             return;
         }
