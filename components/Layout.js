84b3b142651d4e29308f8a45479f3783fb0c9107
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-04-30 23:03:59 +0100

    fiz menu backdropClick

--- components/Layout.js
+++ components/Layout.js
@@ -1,201 +1,214 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
 import {
-    AppBar,
-    Badge,
-    Box,
-    Button,
-    Container,
-    CssBaseline,
-    IconButton,
-    InputBase,
-    Link,
-    Menu,
-    MenuItem,
-    Switch,
-    ThemeProvider,
-    Toolbar,
-    Typography,
-    useMediaQuery,
-} from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
-import Head from 'next/head';
-import NextLink from 'next/link';
-import classes from '../utils/classes';
-import { useContext, useState } from 'react';
-import { Store } from '../utils/Store';
-import jsCookie from 'js-cookie';
-import { useRouter } from 'next/router';
+  AppBar,
+  Badge,
+  Box,
+  Button,
+  Container,
+  CssBaseline,
+  IconButton,
+  InputBase,
+  Link,
+  Menu,
+  MenuItem,
+  Switch,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import Head from "next/head";
+import NextLink from "next/link";
+import classes from "../utils/classes";
+import { useContext, useState } from "react";
+import { Store } from "../utils/Store";
+import jsCookie from "js-cookie";
+import { useRouter } from "next/router";
 
 export default function Layout({ title, description, children }) {
-    const router = useRouter();
-    const { state, dispatch } = useContext(Store);
-    const { darkMode, cart, userInfo } = state;
-    const theme = createTheme({
-        components: {
-            MuiLink: {
-                defaultProps: {
-                    underline: 'hover',
-                },
-            },
+  const router = useRouter();
+  const { state, dispatch } = useContext(Store);
+  const { darkMode, cart, userInfo } = state;
+  const theme = createTheme({
+    components: {
+      MuiLink: {
+        defaultProps: {
+          underline: "hover",
         },
-        typography: {
-            h1: {
-                fontSize: '1.6rem',
-                fontWeight: 400,
-                margin: '1rem 0',
-            },
-            h2: {
-                fontSize: '1.4rem',
-                fontWeight: 400,
-                margin: '1rem 0',
-            },
-        },
-        palette: {
-            mode: darkMode ? 'dark' : 'light',
-            primary: {
-                main: '#929192',
-            },
-            secondary: {
-                main: '#ebe8e3',
-            },
-        },
-    });
-    const darkModeChangeHandler = () => {
-        dispatch({ type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON' });
-        const newDarkMode = !darkMode;
-        jsCookie.set('darkMode', newDarkMode ? 'ON' : 'OFF');
-    };
-    const [anchorEl, setAnchorEl] = useState(null);
-    const loginMenuCloseHandler = (_e, redirect) => {
-        setAnchorEl(null);
-        if (redirect) {
-            router.push(redirect);
-        }
-    };
-    const loginClickHandler = (e) => {
-        setAnchorEl(e.currentTarget);
-    };
-    const logoutClickHandler = () => {
-        setAnchorEl(null);
-        dispatch({ type: 'USER_LOGOUT' });
-        jsCookie.remove('userInfo');
-        jsCookie.remove('cartItems');
-        jsCookie.remove('paymentMethod');
-        router.push('/');
-    };
+      },
+    },
+    typography: {
+      h1: {
+        fontSize: "1.6rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+      h2: {
+        fontSize: "1.4rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+    },
+    palette: {
+      mode: darkMode ? "dark" : "light",
+      primary: {
+        main: "#929192",
+      },
+      secondary: {
+        main: "#ebe8e3",
+      },
+    },
+  });
+  const darkModeChangeHandler = () => {
+    dispatch({ type: darkMode ? "DARK_MODE_OFF" : "DARK_MODE_ON" });
+    const newDarkMode = !darkMode;
+    jsCookie.set("darkMode", newDarkMode ? "ON" : "OFF");
+  };
+  const [anchorEl, setAnchorEl] = useState(null);
+  const loginMenuCloseHandler = (_e, redirect, backdropClick) => {
+    setAnchorEl(null);
+    if (redirect) {
+      router.push(redirect);
+    }
+    if (backdropClick) {
+      router.push(backdropClick);
+    }
+  };
+  const loginClickHandler = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+  const logoutClickHandler = () => {
+    setAnchorEl(null);
+    dispatch({ type: "USER_LOGOUT" });
+    jsCookie.remove("userInfo");
+    jsCookie.remove("cartItems");
+    jsCookie.remove("paymentMethod");
+    router.push("/");
+  };
 
-    const isDesktop = useMediaQuery('(min-width:600px)');
+  const isDesktop = useMediaQuery("(min-width:600px)");
 
-    const [query, setQuery] = useState('');
-    const queryChangeHandler = (e) => {
-        setQuery(e.target.value);
-    };
-    const submitHandler = (e) => {
-        e.preventDefault();
-        router.push(`/search?query=${query}`);
-    };
+  const [query, setQuery] = useState("");
+  const queryChangeHandler = (e) => {
+    setQuery(e.target.value);
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    router.push(`/search?query=${query}`);
+  };
 
-    return (
-        <>
-            <Head>
-                <title>{title ? `${title} - CICLO | Bilheteira` : 'CICLO | Bilheteira'}</title>
-                {description && <meta name="description" content={description}></meta>}
-            </Head>
-            <ThemeProvider theme={theme}>
-                <CssBaseline />
-                <AppBar position="static" sx={classes.appbar}>
-                    <Toolbar sx={classes.toolbar}>
-                        <Box display="flex" alignItems="center">
-                            <NextLink href="/" passHref>
-                                <Link>
-                                    <Typography sx={classes.brand}>CICLO</Typography>
-                                </Link>
-                            </NextLink>
-                        </Box>
-                        <Box sx={isDesktop ? classes.visible : classes.hidden}>
-                            <form onSubmit={submitHandler}>
-                                <Box sx={classes.searchForm}>
-                                    <InputBase
-                                        name="query"
-                                        sx={classes.searchInput}
-                                        placeholder="Pesquisar espetáculos"
-                                        onChange={queryChangeHandler}
-                                    />
-                                    <IconButton
-                                        type="submit"
-                                        sx={classes.searchButton}
-                                        aria-label="search"
-                                    >
-                                        <SearchIcon />
-                                    </IconButton>
-                                </Box>
-                            </form>
-                        </Box>
-                        <Box>
-                            <Switch checked={darkMode} onChange={darkModeChangeHandler}></Switch>
-                            <NextLink href="/cart" passHref>
-                                <Link>
-                                    <Typography component="span">
-                                        {cart.cartItems.length > 0 ? (
-                                            <Badge
-                                                color="secondary"
-                                                badgeContent={cart.cartItems.length}
-                                            >
-                                                Carrinho
-                                            </Badge>
-                                        ) : (
-                                            'Cart'
-                                        )}
-                                    </Typography>
-                                </Link> 
-                            </NextLink>
-                            {userInfo ? (
-                                <>
-                                    <Button
-                                        aria-controls="simple-menu"
-                                        aria-haspopup="true"
-                                        sx={classes.navbarButton}
-                                        onClick={loginClickHandler}
-                                    >
-                                        {userInfo.name}
-                                    </Button>
-                                    <Menu
-                                        id="simple-menu"
-                                        anchorEl={anchorEl}
-                                        keepMounted
-                                        open={Boolean(anchorEl)}
-                                        onClose={loginMenuCloseHandler}
-                                    >
-                                        <MenuItem
-                                            onClick={(e) => loginMenuCloseHandler(e, '/profile')}
-                                        >
-                                            Perfil
-                                        </MenuItem>
-                                        <MenuItem
-                                            onClick={(e) =>
-                                                loginMenuCloseHandler(e, '/order-history')
-                                            }
-                                        >
-                                            Histórico de reservas
-                                        </MenuItem>
-                                        <MenuItem onClick={logoutClickHandler}>Sair</MenuItem>
-                                    </Menu>
-                                </>
-                            ) : (
-                                <NextLink href="/login" passHref>
-                                    <Link>Iniciar Sessão</Link>
-                                </NextLink>
-                            )}
-                        </Box>
-                    </Toolbar>
-                </AppBar>
-                <Container component="main" sx={classes.main}>
-                    {children}
-                </Container>
-                <Box component="footer" sx={classes.footer}>
-                    <Typography>Escola de Teatro da Retorta | Desenvolvido por Nuno Miguel Ferraz</Typography>
+  return (
+    <>
+      <Head>
+        <title>
+          {title ? `${title} - CICLO | Bilheteira` : "CICLO | Bilheteira"}
+        </title>
+        {description && <meta name="description" content={description}></meta>}
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static" sx={classes.appbar}>
+          <Toolbar sx={classes.toolbar}>
+            <Box display="flex" alignItems="center">
+              <NextLink href="/" passHref>
+                <Link>
+                  <Typography sx={classes.brand}>CICLO</Typography>
+                </Link>
+              </NextLink>
+            </Box>
+            <Box sx={isDesktop ? classes.visible : classes.hidden}>
+              <form onSubmit={submitHandler}>
+                <Box sx={classes.searchForm}>
+                  <InputBase
+                    name="query"
+                    sx={classes.searchInput}
+                    placeholder="Pesquisar espetáculos"
+                    onChange={queryChangeHandler}
+                  />
+                  <IconButton
+                    type="submit"
+                    sx={classes.searchButton}
+                    aria-label="search"
+                  >
+                    <SearchIcon />
+                  </IconButton>
                 </Box>
-            </ThemeProvider>
-        </>
-    )
+              </form>
+            </Box>
+            <Box>
+              <Switch
+                checked={darkMode}
+                onChange={darkModeChangeHandler}
+              ></Switch>
+              <NextLink href="/cart" passHref>
+                <Link>
+                  <Typography component="span">
+                    {cart.cartItems.length > 0 ? (
+                      <Badge
+                        color="secondary"
+                        badgeContent={cart.cartItems.length}
+                      >
+                        Carrinho
+                      </Badge>
+                    ) : (
+                      "Cart"
+                    )}
+                  </Typography>
+                </Link>
+              </NextLink>
+              {userInfo ? (
+                <>
+                  <Button
+                    aria-controls="simple-menu"
+                    aria-haspopup="true"
+                    sx={classes.navbarButton}
+                    onClick={loginClickHandler}
+                  >
+                    {userInfo.name}
+                  </Button>
+                  <Menu
+                    id="simple-menu"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    backdrop="true"
+                    hideBackdrop="false"
+                    open={Boolean(anchorEl)}
+                    onClose={loginMenuCloseHandler}
+                  >
+                    <MenuItem
+                      onClick={(e) => loginMenuCloseHandler(e, "/profile")}
+                    >
+                      Fechar menu
+                    </MenuItem>
+                    <MenuItem onClick={() => router.back()}>Perfil</MenuItem>
+                    <MenuItem
+                      onClick={(e) =>
+                        loginMenuCloseHandler(e, "/order-history")
+                      }
+                    >
+                      Histórico de reservas
+                    </MenuItem>
+                    <MenuItem onClick={logoutClickHandler}>Sair</MenuItem>
+                  </Menu>
+                </>
+              ) : (
+                <NextLink href="/login" passHref>
+                  <Link>Iniciar Sessão</Link>
+                </NextLink>
+              )}
+            </Box>
+          </Toolbar>
+        </AppBar>
+        <Container component="main" sx={classes.main}>
+          {children}
+        </Container>
+        <Box component="footer" sx={classes.footer}>
+          <Typography>
+            Escola de Teatro da Retorta | Desenvolvido por Nuno Miguel Ferraz
+          </Typography>
+        </Box>
+      </ThemeProvider>
+    </>
+  );
 }
