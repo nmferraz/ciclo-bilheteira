2f7f05324367f08bbc9017f1e6cfd629900418c1
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-06-10 12:50:01 +0100

    google analitycs

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,50 @@
-import '../styles/globals.css'
-import createCache from '@emotion/cache'
-import { CacheProvider } from '@emotion/react'
-import { StoreProvider } from '../utils/Store'
-import { SnackbarProvider } from 'notistack'
-import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+import "../styles/globals.css";
+import Script from "next/script";
+import createCache from "@emotion/cache";
+import { CacheProvider } from "@emotion/react";
+import { StoreProvider } from "../utils/Store";
+import { SnackbarProvider } from "notistack";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
-const clientSideEmotionCache = createCache({ key: 'css' })
+const clientSideEmotionCache = createCache({ key: "css" });
 
-function MyApp({ Component, pageProps, emotionCache = clientSideEmotionCache }) {
+function MyApp({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache,
+}) {
   return (
-    <CacheProvider value={emotionCache}>
-      <SnackbarProvider anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
-        <StoreProvider>
-          <PayPalScriptProvider deferLoading={true}>
-            <Component {...pageProps} />
-          </PayPalScriptProvider>
-        </StoreProvider>
-      </SnackbarProvider>
-    </CacheProvider>
-  )
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      {/*eslint-disable-next-line @next/next/inline-script-id*/}
+      <Script strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
+
+      <CacheProvider value={emotionCache}>
+        <SnackbarProvider
+          anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        >
+          <StoreProvider>
+            <PayPalScriptProvider deferLoading={true}>
+              <Component {...pageProps} />
+            </PayPalScriptProvider>
+          </StoreProvider>
+        </SnackbarProvider>
+      </CacheProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
